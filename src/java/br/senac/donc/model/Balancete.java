909f3f9aeb2aca18d23a08524b2660251153e1f4
danilosetubal <danilosetubal@gmail.com>
2017-09-28 09:39:56 -0300

    pathFile included in variable

--- src/java/br/senac/donc/model/Balancete.java
+++ src/java/br/senac/donc/model/Balancete.java
@@ -11,6 +11,37 @@ package br.senac.donc.model;
  */
 public class Balancete {
     
+    private String dataInicial;
+    private String dataFinal;
+    private Double cotaMensal;
+
+    public Balancete() {
+    }
+
+    public String getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(String dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public String getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Double getCotaMensal() {
+        return cotaMensal;
+    }
+
+    public void setCotaMensal(Double cotaMensal) {
+        this.cotaMensal = cotaMensal;
+    }
+    
     
     
 }
