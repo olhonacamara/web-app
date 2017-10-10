528e009f8363f2ae6181e5306e677cdcc00193d9
Tiago Meurer <tiagomeurersc@gmail.com>
2017-10-10 09:36:18 -0300

    melhorias na classe de leitura

--- src/java/br/senac/donc/util/Leitura.java
+++ src/java/br/senac/donc/util/Leitura.java
@@ -21,40 +21,36 @@ public class Leitura {
         Balancete balancete = new Balancete();
         Vereador vereador = new Vereador();
         int cont = 0;
+        int contVereador = 0;
 
         //como ler através da contagem de linhas?:
         //String line22 = Files.readAllLines(Paths.get("/Volumes/Macintosh_HD/tiagomeurer/Desktop/test.txt")).get(21);
         //inserir o caminho do arquivo .txt no disco
         String caminho = "/Volumes/Macintosh_HD/Users/tiagomeurer/Desktop/test.txt";
 
-        System.out.printf("\nConteúdo do arquivo texto:\n");
+        System.out.printf("\nConteúdo do arquivo extraido do .txt:\n");
         try {
             //o objeto arq lê o arquivo .txt
             FileReader arq = new FileReader(caminho);
             //aqui ele armazena o conteudo na memoria
             BufferedReader lerArq = new BufferedReader(arq);
 
-            String linha=null; //= lerArq.readLine(); 
-            
-// lê a primeira linha
-// a variável "linha" recebe o valor "null" quando o processo
-// de repetição atingir o final do arquivo texto
+            String linha = null; //= lerArq.readLine(); 
 
+            // lê a primeira linha
+            // a variável "linha" recebe o valor "null" quando o processo
+            // de repetição atingir o final do arquivo texto
             do {
                 linha = lerArq.readLine(); // lê da segunda até a última linha
 
-                if (linha!=null){
-                    
+                if (linha != null) {
+                    if (linha.equals("Vereador :")) {
+                        cont++;
+                        System.out.println("");
+                        System.out.println("Pagina " + cont);
+                    }
 
-                if (linha.equals("Vereador :")) {
-                    cont++;
-                    System.out.println(cont);
-                }
-
-                
-                             
-
-                    //               captura do periodo 
+                    //captura do periodo 
                     {
                         if (linha.equals("Período :")) {
                             balancete.setDataInicial(lerArq.readLine());
@@ -71,7 +67,8 @@ public class Leitura {
                         //pulando uma linha pois a linha apos Vereador é o codigo do vereador
                         lerArq.readLine();
                         vereador.setNomeCompleto(lerArq.readLine());
-                        System.out.println("Valor capturado de vereador: " + vereador.getNomeCompleto());
+                        System.out.println("Vereador: " + vereador.getNomeCompleto());
+                        contVereador++;
                     }
 
                     //captura gastos cota mensal
@@ -86,9 +83,8 @@ public class Leitura {
 
                     }
 
-                    //CAPTURANDO LINHA OUTROSv
+                    //Capturando a linha *OUTROS*
                     if (linha.equals("OUTROS")) {
-
                         String outros = lerArq.readLine();
                         outros = outros.replace(",", "");
                         Double outrosDouble = (Double.parseDouble(outros));
@@ -96,7 +92,7 @@ public class Leitura {
                         System.out.println("Valor capturado de COTA: " + balancete.getOutros1());
                     }
 
-                    //captura CREDITO ANO ANTERIOR
+                    //captura a linha *CREDITO ANO ANTERIOR*
                     if (linha.equals("OUTROS")) {
                         lerArq.readLine();
                         String anoAnterior = lerArq.readLine();
@@ -106,7 +102,7 @@ public class Leitura {
                         System.out.println("Valor capturado de CREDITO DE ANO ANTERIOR: " + balancete.getAnoAnterior());
                     }
 
-                    //capturando a linha DEBITOS
+                    //capturando a linha *DEBITOS*
                     if (linha.equals("Credito ano anterior")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -117,7 +113,7 @@ public class Leitura {
                         System.out.println("Valor capturado de DÉBITOS: " + balancete.getDebitos());
                     }
 
-                    //capturando a linha BOLSA/ESTAGIO
+                    //capturando a linha *BOLSA/ESTAGIO*
                     if (linha.equals("DEBITOS")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -128,7 +124,7 @@ public class Leitura {
                         System.out.println("Valor capturado de BOLSA/ESTAGIO: " + balancete.getBolsaEstagio());
                     }
 
-                    //capturando a linha PASSAGENS
+                    //capturando a linha *PASSAGENS*
                     if (linha.equals("Bolsa / Estágio")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -139,7 +135,7 @@ public class Leitura {
                         System.out.println("Valor capturado de PASSAGENS: " + balancete.getPassagens());
                     }
 
-                    //capturando a linha Telefone Fixo (GVT)
+                    //capturando a linha *Telefone Fixo (GVT)*
                     if (linha.equals("Passagens")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -150,7 +146,7 @@ public class Leitura {
                         System.out.println("Valor capturado de TELEFONE FIXO: " + balancete.getTelefoneFixo());
                     }
 
-                    //capturando a linha XEROX
+                    //capturando a linha *XEROX*
                     if (linha.equals("Telefone Fixo (GVT)")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -161,7 +157,7 @@ public class Leitura {
                         System.out.println("Valor capturado de XEROX: " + balancete.getXerox());
                     }
 
-                    //capturando a linha CORREIO
+                    //capturando a linha *CORREIO*
                     if (linha.equals("Xerox")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -172,7 +168,7 @@ public class Leitura {
                         System.out.println("Valor capturado de CORREIO: " + balancete.getCorreio());
                     }
 
-                    //capturando a linha Material de Expediente
+                    //capturando a linha *Material de Expediente*
                     if (linha.equals("Correio")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -183,7 +179,7 @@ public class Leitura {
                         System.out.println("Valor capturado de MATERIAL DE EXPEDIENTE: " + balancete.getMaterialExpediente());
                     }
 
-                    //capturando a linha Diarias
+                    //capturando a linha *Diarias*
                     if (linha.equals("Material de Expediente")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -194,7 +190,7 @@ public class Leitura {
                         System.out.println("Valor capturado de DIARIAS: " + balancete.getDiarias());
                     }
 
-                    //capturando a linha Gratificação
+                    //capturando a linha *Gratificação*
                     if (linha.equals("Diárias")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -205,7 +201,7 @@ public class Leitura {
                         System.out.println("Valor capturado de GRATIFICAÇÃO: " + balancete.getGratificacao());
                     }
 
-                    //capturando a linha OUTROS
+                    //capturando a linha *OUTROS*
                     if (linha.equals("Gratificação")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -216,7 +212,7 @@ public class Leitura {
                         System.out.println("Valor capturado de OUTROS: " + balancete.getOutros2());
                     }
 
-                    //capturando a linha Selos
+                    //capturando a linha *Selos*
                     if (linha.equals("2.09")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -228,7 +224,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Selos: " + balancete.getSelos());
                     }
 
-                    //capturando a linha Serviços
+                    //capturando a linha *Serviços*
                     if (linha.equals("Selos")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -239,7 +235,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Serviços: " + balancete.getServicos());
                     }
 
-                    //capturando a linha Telefone TIM
+                    //capturando a linha *Telefone TIM*
                     if (linha.equals("Serviços")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -250,7 +246,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Telefone - TIM: " + balancete.getTelefoneMovel());
                     }
 
-                    //capturando a linha Cursos
+                    //capturando a linha *Cursos*
                     if (linha.equals("Telefone - TIM")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -261,7 +257,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Cursos: " + balancete.getCursos());
                     }
 
-                    //capturando a linha Carimbos
+                    //capturando a linha *Carimbos*
                     if (linha.equals("Cursos")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -272,19 +268,22 @@ public class Leitura {
                         System.out.println("Valor capturado de Carimbos: " + balancete.getCarimbos());
                     }
 
-//                //capturando a linha Ligação Interurbano (B.TELECOM/EMBRATEL)
-                
-//                    if (linha.equals("Débitos no Mês :")) {
-//                        lerArq.readLine();
-//                        lerArq.readLine();
-//                        String debitoAnoAnterior = lerArq.readLine();
-//                        debitoAnoAnterior = debitoAnoAnterior.replace(",", "");
-//                        Double debitoAnoAnteriorDouble = (Double.parseDouble(debitoAnoAnterior));
-//                        balancete.setDebitoAnoAnterior(debitoAnoAnteriorDouble);
-//                        System.out.println("Valor capturado de Débito ano anterior: " + balancete.getDebitoAnoAnterior());
-//                    }
-//                    
-                    //capturando a linha Débito ano anterior
+//                //capturando a linha *Ligação Interurbano (B.TELECOM/EMBRATEL)*
+                    if (linha.equals("Carimbos")) {
+                        int i = 0;
+                        if (i < 8) {
+                            lerArq.readLine();
+
+                        }
+                        String interurbano = lerArq.readLine();
+                        interurbano = interurbano.replace(",", "");
+                        Double interurbanoDouble = (Double.parseDouble(interurbano));
+                        balancete.setInterurbano(interurbanoDouble);
+                        System.out.println("Valor capturado de Ligação Interurbano (B.TELECOM/EMBRATEL): " + balancete.getInterurbano());
+                        i = i++;
+                    }
+
+                    //capturando a linha *Débito ano anterior*
                     if (linha.equals("Ligação Interurbano (B.TELECOM/EMBRATEL)")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -295,7 +294,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Débito ano anterior: " + balancete.getDebitoAnoAnterior());
                     }
 
-                    //capturando a linha Jornal/Revista
+                    //capturando a linha *Jornal/Revista*
                     if (linha.equals("Débito ano anterior")) {
                         lerArq.readLine();
                         lerArq.readLine();
@@ -306,7 +305,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Jornal/Revista: " + balancete.getJornalRevista());
                     }
 
-                    //capturando a linha Saldo anterior
+                    //capturando a linha *Saldo anterior*
                     if (linha.equals("Saldo Anterior :")) {
                         String saldoAnterior = lerArq.readLine();
                         saldoAnterior = saldoAnterior.replace(",", "");
@@ -317,7 +316,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Saldo anterior: " + balancete.getAnoAnterior());
                     }
 
-                    //capturando a linha Saldo Acumulado
+                    //capturando a linha *Saldo Acumulado*
                     if (linha.equals("Saldo Acumulado:")) {
                         String saldoAcumulado = lerArq.readLine();
                         saldoAcumulado = saldoAcumulado.replace(",", "");
@@ -328,7 +327,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Saldo Acumulado: " + balancete.getSaldoAcumulado());
                     }
 
-                    //capturando a linha creditos no Mes
+                    //capturando a linha *Creditos no Mes*
                     if (linha.equals("Créditos no Mês :")) {
                         lerArq.readLine();
                         String creditosMes = lerArq.readLine();
@@ -338,7 +337,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Creditos Mês: " + balancete.getCreditosMes());
                     }
 
-                    //capturando a linha Debitos no Mes
+                    //capturando a linha *Debitos no Mes*
                     if (linha.equals("Débitos no Mês :")) {
                         String debitosMes = lerArq.readLine();
                         debitosMes = debitosMes.replace(",", "");
@@ -347,7 +346,7 @@ public class Leitura {
                         System.out.println("Valor capturado de Debitos Mês: " + balancete.getDebitosMes());
 
                     }
-            }
+                }
 
 
             } while (linha != null);
@@ -357,7 +356,9 @@ public class Leitura {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
                     e.getMessage());
         }
-
-        System.out.println();
+        //imprime a quantidade de vezes que o codigo passou pela captura de vereadores
+        System.out.println("O codigo percorreu um total de " + contVereador + " vereadores.");
+        //criar um metodo para realizar a conversão de String para Double   
+        //criar método para o replace de "," para ""
     }
 }
